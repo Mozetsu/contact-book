3edf3a97605e47e3351a1cf256bd27b50127acac
Mozetsu <hello@mozetsu.dev>
2022-04-16 19:24:06 +0100

    feat: implement database seeder

---
+++ database/seeders/ContactTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+use DB;
+
+class ContactTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        # Remove all existing entrie
+        DB::table('contacts')->delete() ;
+
+        Contact::create([
+            'name' => 'ice',
+            'email' => 'ice@domain.io',
+            'phone' => 111111111,
+            'country' => "iceland",
+        ]);
+
+        Contact::create([
+            'name' => 'fire',
+            'email' => 'fire@domain.io',
+            'phone' => 222222222,
+            'country' => "norway",
+        ]);
+
+        Contact::create([
+            'name' => 'water',
+            'email' => 'water@domain.io',
+            'phone' => 333333333,
+            'country' => "Mexico",
+        ]);
+
+        Contact::create([
+            'name' => 'earth',
+            'email' => 'earth@domain.io',
+            'phone' => 444444444,
+            'country' => "Japan",
+        ]);
+
+        // factory(Contact::class)->times(3)->create();
+    }
+}
