3eb4fd0a1ddb8401e66b4043acf78260db342b15
Mozetsu <hello@mozetsu.dev>
2022-04-17 04:20:52 +0100

    refactor: update dev varible

--- public/js/app.js
+++ public/js/app.js
@@ -16416,6 +16416,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
+/* provided dependency */ var process = __webpack_require__(/*! process/browser.js */ "./node_modules/process/browser.js");
 
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
@@ -16423,7 +16424,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 
-var API_BASE = "http://127.0.0.1:8000/api/";
+var API_BASE = process.env.API_BASE || "http://127.0.0.1:8000/api/";
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   FetchContacts: function () {
     var _FetchContacts = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
--- resources/js/controllers/api.js
+++ resources/js/controllers/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = "http://127.0.0.1:8000/api/";
+const API_BASE = process.env.API_BASE || "http://127.0.0.1:8000/api/";
 
 export default {
     FetchContacts: async () => {
